19958b7596413e90700aa66674fb7d2849be5d90
Jawad <abdeljawadalami@gmail.com>
2018-02-14 13:57:20 +0200

    1st commit

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include "welcome.h"
+#include <QApplication>
+#include <QDesktopWidget>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Welcome w;
+
+    ////////////////////////////////////////////////////////
+    /// \brief setting window in the middle of the screen
+    ////////////////////////////////////////////////////////
+    int width = w.frameGeometry().width();
+    int height = w.frameGeometry().height();
+
+    QDesktopWidget wid;
+
+    int screenWidth = wid.screen()->width();
+    int screenHeight = wid.screen()->height();
+
+    w.setGeometry((screenWidth/2)-(width/2),(screenHeight/2)-(height/2),width,height);
+    /////////////////////////////////////////////////////////
+
+    w.show();
+
+    return a.exec();
+}
